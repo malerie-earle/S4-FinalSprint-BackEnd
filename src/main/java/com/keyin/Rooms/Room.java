c549c1bf2914fcfeb15e0fa525959d953bafbcca
NLScotty <Scott.B.Normore@gmail.com>
2024-07-29 17:59:44 -0230

    Room Completed
    To do: Room booking

--- src/main/java/com/keyin/Rooms/Room.java
+++ src/main/java/com/keyin/Rooms/Room.java
@@ -1,4 +1,68 @@
 package com.keyin.Rooms;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
 public class Room {
+
+    @Id
+    @SequenceGenerator(name = "room_sequence", sequenceName = "room_sequence", allocationSize = 1, initialValue=1)
+    @GeneratedValue(generator = "room_sequence")
+    private long room_id;
+    private int room_number;
+    private String type;
+    private String description;
+    private int occupancy;
+    private String image;
+
+    public long getRoom_id() {
+        return room_id;
+    }
+
+    public void setRoom_id(long room_id) {
+        this.room_id = room_id;
+    }
+
+    public int getRoom_number() {
+        return room_number;
+    }
+
+    public void setRoom_number(int room_number) {
+        this.room_number = room_number;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getOccupancy() {
+        return occupancy;
+    }
+
+    public void setOccupancy(int occupancy) {
+        this.occupancy = occupancy;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
