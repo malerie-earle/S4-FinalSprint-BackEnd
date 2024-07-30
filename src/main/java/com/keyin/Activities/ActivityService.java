e1f6bd6a81d6a7e25d073232feeec062c5c38041
Corina Jewer <corina.jewer@keyin.com>
2024-07-30 11:20:06 -0230

    Updated attributes for Activity, User, & Activity_Booking Classes. Completed CRUD functionality for Activity endpoints.

---
+++ src/main/java/com/keyin/Activities/ActivityService.java
@@ -0,0 +1,26 @@
+package com.keyin.Activities;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ActivityService {
+
+    @Autowired
+    private ActivityRepository activityRepository;
+
+    public List<Activity> getAllActivities() {
+        return (List<Activity>) activityRepository.findAll();
+    }
+
+    public Optional<Activity> getActivityById(Long activityId) {
+            return activityRepository.findById(activityId);
+    }
+
+    public Activity createActivity(Activity newActivity) {
+        return activityRepository.save(newActivity);
+    }
+}
