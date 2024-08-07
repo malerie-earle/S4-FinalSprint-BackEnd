fd5295d3c75ba856987ee59abc1bf4481bae9ad4
Malerie Earle <malerie.augot@gmail.com>
2024-08-07 17:29:09 -0230

    old review code commented and added to bottom

--- src/main/java/com/keyin/Reviews/ReviewService.java
+++ src/main/java/com/keyin/Reviews/ReviewService.java
@@ -35,3 +35,27 @@ public class ReviewService {
         reviewRepository.save(review);
     }
 }
+//package com.keyin.Reviews;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Service;
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//@@ -10,18 +12,26 @@ public class ReviewService {
+//    @Autowired
+//    private ReviewRepository reviewRepository;
+//
+//    public List <ReviewResponseDTO> getAllUserReviews(){
+//        List<Review> userReviews = reviewRepository.findAll();
+//        List<ReviewResponseDTO> reviews = new ArrayList<>();
+//        for (Review review : userReviews) {
+//            ReviewResponseDTO responseDTO = new ReviewResponseDTO();
+//            responseDTO.setFName(review.getUser().getFName());
+//            responseDTO.setLName(review.getUser().getLName());
+//            responseDTO.setRating(review.getRating());
+//            responseDTO.setText(review.getText());
+//            reviews.add(responseDTO);
+//        }
+//        return reviews;
+//    }
+//}
