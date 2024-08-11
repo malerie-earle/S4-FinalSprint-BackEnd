be49be6ff13b3fc4885fdb3ea1f09c633bd7c8fa
NLScotty <Scott.B.Normore@gmail.com>
2024-08-11 17:43:59 -0230

    Update WebConfig.java

--- src/main/java/com/keyin/Config/WebConfig.java
+++ src/main/java/com/keyin/Config/WebConfig.java
@@ -13,7 +13,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
                 .allowedOriginPatterns("*") // Replace with your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
